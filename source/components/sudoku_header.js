e22231fdeeaa31524c2146141b05bb7258d0f9ae
Fuji Haruka <fizzyjohn316@gmail.com>
2016-04-30 18:05:47 +0900

    delete dev

--- source/components/sudoku_header.js
+++ source/components/sudoku_header.js
@@ -2,8 +2,6 @@ import React from 'react'
 import difficultyStore from '../stores/difficulty_store'
 import {DIFFICUITY} from '../util/consts'
 
-import sudokuFinishStore from '../stores/sudoku_finish_store'
-
 const SudokuHeader = React.createClass({
   render () {
     let difficulty = difficultyStore.getState()
@@ -11,13 +9,10 @@ const SudokuHeader = React.createClass({
     let month = today.getMonth() + 1
     let date = today.getDate()
     return (
-      <div className='sudoku-header-message' onClick={this.onClick}>
+      <div className='sudoku-header-message'>
         {month}月{date}日のナンプレ問題 [ 難易度 : {DIFFICUITY[difficulty]} ]
       </div>
     )
-  },
-  onClick () {
-    sudokuFinishStore.finish()
   }
 })
 
